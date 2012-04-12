dae69f2576845a9f282452ad9c3b7a0aefb9f373
Vivek Parihar <vivek@weboniselab.com>
2012-04-12 13:58:25 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CrowdFunding::Application.config.secret_token = '68ab9a27960fb1cb0fb057e89f08c0d50443d88d2b422a3f4f2b314c6167ff21b998d17e10051a41eac0febc9bae619ec283c1a68b904d223ccbb2799f0af70d'
