6d8e2e5405a3f2c2d095d014c471483dd379b24d
Vivek Parihar <vivek@weboniselab.com>
2012-04-16 13:44:19 +0530

    changes in new_letter.html.rb

--- config/application.rb
+++ config/application.rb
@@ -66,5 +66,15 @@ module CrowdFunding
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    config.action_mailer.delivery_method = :smtp
+    config.action_mailer.smtp_settings = {
+        :address              => "smtp.gmail.com",
+        :port                 => 587,
+        :domain               => 'mail.weboniselab.com',
+        :user_name            => 'pansingh@weboniselab.com',
+        :password             => 'pansingh6186',
+        :authentication       => 'plain',
+        :enable_starttls_auto => true  }
   end
 end
