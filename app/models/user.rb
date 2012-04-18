d0405a2f699f9d506450c7f5eda7d5f55a004e3b
Vivek Parihar <vivek@weboniselab.com>
2012-04-18 16:40:04 +0530

    validation differs for facebook user and simple user while registration

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,6 +22,7 @@ class User
   field :current_sign_in_ip, :type => String
   field :last_sign_in_ip,    :type => String
   field :suspended,          :type => Boolean ,:null => false, :default => false
+  field :is_provider,        :type => Boolean ,:null => false, :default => false
 
 
   ## Encryptable
@@ -45,13 +46,15 @@ class User
 
   def self.find_for_facebook_oauth(access_token, signed_in_resource=nil)
     data = access_token.extra.raw_info
-    logger.info("received from Facebook: #{data[:first_name]}")
+    logger.info("received from Facebook: #{data.inspect}")
     user = User.where(:email => data.email).first
+
     if !user.nil?
       user
     else # Create an user with a stub password.
       user = User.new({:email => data["email"],
                        :password => Devise.friendly_token[0,20],
+                       :is_provider => true,
                        :profile => {:first_name => data["first_name"],:last_name => data["last_name"]}})
       user.confirm!
       user.save!
@@ -97,18 +100,24 @@ class User
 #
   field :country,            :type => String
   validates :country,
-            :presence => true
+            :presence => true,
+            :if => :should_not_provider?
 
   field :terms_of_service,   :type => Boolean
   validates :terms_of_service,
             :acceptance => {:accept => true},
-            :on => :create
+            :on => :create,
+            :if => :should_not_provider?
+
 
   embeds_one :profile
   accepts_nested_attributes_for :profile
 
-  attr_accessible :profile, :email, :password, :password_confirmation, :remember_me ,:country, :terms_of_service
+  attr_accessible :profile, :email, :password, :password_confirmation, :remember_me ,:country, :terms_of_service,:is_provider
 
+  def should_not_provider?
+    is_provider == false
+  end
 
 end
 
