f6fa20b478ccf444d7a958bf25b3ee4d1ddd1d36
webonise <webonise@webonise-Invalid-entry-length-0-DMI-table-is-broken-Stop.(none)>
2012-05-18 15:03:31 +0530

    changes for invalid time issue

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -6,6 +6,7 @@ class ProfilesController < ApplicationController
     @profile = Profile.new
 #    @profile.inspect
   end
+
   def create
     profile = @user.build_profile(params[:profile])
     logger.info "##########################{profile.inspect}"
@@ -18,8 +19,10 @@ class ProfilesController < ApplicationController
   def edit
     @profile = current_user.profile
   end
+
   def update
     @profile = current_user.profile
+    params[:profile][:birth_date] = format_birth_date(params[:profile][:birth_date])
    if @profile.update_attributes(params[:profile])
      redirect_to root_path ,:notice => "profile updated successfully"
    end
@@ -30,4 +33,10 @@ class ProfilesController < ApplicationController
 #  def record_not_found
 #    flash[:error] = 'The team you requested could not be found.'
 #  end
+  private
+
+  def format_birth_date birthdate
+    birthdate_array= birthdate.split("/")
+    birthdate_array[1] +"-#{birthdate_array[0]}"+"-#{birthdate_array[2]}"
+  end
 end
