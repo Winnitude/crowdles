268885059ecb6530e3ba46db97c9a95aa30f95ce
Kunal Bhatia <kunalb@weboniselab.com>
2012-06-05 17:45:34 +0530

    soleve the bug with role changed

--- app/controllers/main_admin_group_owners_controller.rb
+++ app/controllers/main_admin_group_owners_controller.rb
@@ -22,17 +22,22 @@ class MainAdminGroupOwnersController < ApplicationController
     group = MainAdminGroup.where(:country => current_user.la_setting.la_country).to_a.first
     user= User.where(:email => params[:worker_email]).to_a.first
     if user.present?
-    existing_owner = group.user
-    logger.info user.inspect
-    group.user = user
-    if group.save
-      RolesManagement::RolesManager.remove_role("Main Admin Group", existing_owner)
-      RolesManagement::RolesManager.add_role("Main Admin Group", user)
-    redirect_to :root , :notice => "changed ownership successfully"
-    end
+      existing_owner = group.user
+      abc = existing_owner
+      RolesManagement::RolesManager.remove_role("Main Admin Group Owner",existing_owner)
+      logger.info user.inspect
+      logger.info("===#{existing_owner}========#{group.user}=========#{user}==============#{user == existing_owner}")
+      group.user = user
+      logger.info("===#{existing_owner}========#{group.user}=========#{user}==============#{user == existing_owner}")
+      logger.info("===#{existing_owner}========#{group.user}=========#{user}==============#{user == existing_owner}")
+      if group.save
+        RolesManagement::RolesManager.add_role("Main Admin Group Owner", user)
+        redirect_to :root , :notice => "changed ownership successfully"
+      end
     else
-    redirect_to :root , :notice => "User not Found"
+      redirect_to :root , :notice => "User not Found"
     end
+
   end
 
   def  should_be_GA       #TODO need to move to user model
