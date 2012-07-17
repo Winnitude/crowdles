3a731984f545e781adc3885012637825dcdfe5ad
Kunal Bhatia <kunalb@weboniselab.com>
2012-07-17 17:23:40 +0530

    deploying

--- app/controllers/pass_billing_profiles_controller.rb
+++ app/controllers/pass_billing_profiles_controller.rb
@@ -8,8 +8,19 @@ class PassBillingProfilesController < ApplicationController
   def update
     @billing_profile = PlatformBillingProfile.find (params[:id])
     #NOTE the following code is to create the MAG for LA when GA create the LA
-   if @billing_profile.update_attributes(params[:platform_billing_profile])
-     redirect_to root_path , :notice => "Updated Successfully"
+
+    if @billing_profile.update_attributes(params[:platform_billing_profile])
+      @admin_group_owner = @billing_profile.la_setting.user
+      @admin_group = @admin_group_owner.build_admin_group(:admin_group_type => "Master")
+      @admin_group.set_group_attributes(@admin_group_owner.la_setting)
+      @admin_group.la_setting = @billing_profile.la_setting
+      @product = Product.where(:type => "Master").first
+      @user_product = @admin_group_owner.user_products.new
+      @user_product.product = @product
+      @admin_group_owner.save && @admin_group.save  && @user_product.save
+      @admin_group_owner.add_role "Main Admin Group Owner"
+      redirect_to edit_main_admin_group_local_admin_path(@admin_group_owner) , :notice => "Updated Successfully"
+    end
    end
 
   end
